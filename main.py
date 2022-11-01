ac1ab07ee716742f0706c398adf4efce442437c2
kmgovind <kavin.gmk@gmail.com>
2022-11-01 17:30:46 -0400

    Created main.py as driver code for robot

---
+++ main.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from signal import pause
+
+# Set Pin Constants
+
+
+# Setup Pins
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+# GPIO.setup()
+
+def main():
+    print("hello")
+
+
+if __name__=="__main__":
+    main()
