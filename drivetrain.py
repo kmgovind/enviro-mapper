2c81077df62f3e7659d3cf964405c90fe1eac2f0
kmgovind <kavin@gmail.com>
2022-11-09 17:27:50 -0500

    Created template drivetrain file

---
+++ drivetrain.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from signal import pause
+import constants as k
+
+# Setup Pins
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD) # using the board's numbering scheme
+GPIO.setup(k.RMotorFW, GPIO.OUT)
+
+# Main Logic
+while True:
+	# Toggle RMotorFW On/Off
+	GPIO.output(k.RMotorFW, True)
+	sleep(1)
+	GPIO.output(k.RMotorFW, False)
+	sleep(1)
